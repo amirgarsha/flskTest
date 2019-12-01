a74c6a6740fc113577dd90233064d1fe9f70e5cf
amir <amir.gr86@gmail.com>
2019-12-01 12:37:48 +0330

    my first flask project

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+@app.route("/members/<string:name>/")
+def getMember(name):
+    return "name</string:name>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
